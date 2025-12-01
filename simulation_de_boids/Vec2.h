344c6702aa06afc6c23aeeb2bd86295311df81b8
Mageallan <alexandremimizan@gmail.com>
2025-12-01 15:37:58 +0100

    Classe Vec2.h

---
+++ simulation_de_boids/Vec2.h
@@ -0,0 +1,30 @@
+#pragma once
+typedef float scal;
+class Vec2 {
+	scal x;
+	scal y;
+
+public:
+	//Choisir un des deux constructeur
+	Vec2();
+	Vec2(scal x, scal y);
+	~Vec2();
+
+	Vec2 operator+(const Vec2& vec);	
+	Vec2 operator-(const Vec2& vec);
+	Vec2 operator*(const Vec2& vec);
+	Vec2 operator/(const Vec2& vec);
+
+	Vec2& operator+=(const Vec2& vec);
+	Vec2& operator-=(const Vec2& vec);
+	Vec2& operator*=(const Vec2& vec);
+	Vec2& operator/=(const Vec2& vec);
+
+	bool operator==(const Vec2& vec);
+	bool operator!=(const Vec2& vec);
+	void operator<<(const Vec2& vec);
+
+
+
+
+};
