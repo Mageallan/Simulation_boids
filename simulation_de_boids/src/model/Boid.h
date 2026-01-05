9e54de601f865629e2deea663eb828f10666b3e9
lukatexier <luka@famille-texier.fr>
2026-01-05 08:29:37 +0100

    ajout du model

---
+++ simulation_de_boids/src/model/Boid.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Vec2.h"
+class Boid {
+	Vec2<double> position;
+	Vec2<double> speed;
+	float perception_radius;
+
+
+public:
+	void position_incrementation(float delta_t);
+	void speed_incrementation(float coef);
+
+};
---
+++ simulation_de_boids/src/model/DynamicArray.h
@@ -0,0 +1,8 @@
+#pragma once
+template<typename T>
+class DynamicArray {
+   
+	int high;
+	int currentCapacity;
+	
+};
